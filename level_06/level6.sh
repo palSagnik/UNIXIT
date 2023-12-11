93fa4864be0d70dc4a971f805a1041b37fe64855
SAGNIK PAL <sagnik@SAGNIKs-MacBook-Air.local>
2023-12-11 22:25:53 +0530

    Second commit

--- level_06/level6.sh
+++ level_06/level6.sh
@@ -2,13 +2,13 @@
 
 cd /home/level6
 cp /root/grep.zip .
-chown level6:level6 grep.zip
-chmod 444 grep.zip
 unzip grep.zip
 mv file9 file1
 mv file8 file2 
+chmod 775 -R /home/level6/*
 cd file3/root/
 touch file
 python3 /root/flag.py
+rm grep.zip
 chattr +i /home/level6
 /usr/sbin/sshd -D
